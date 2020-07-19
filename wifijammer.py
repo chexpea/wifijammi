081bf9dc686ae2ff7c383485f6330dff623d17fa
chexpea <68424626+chexpea@users.noreply.github.com>
2020-07-19 16:33:49 +0300

    Update wifijammer.py

--- wifijammer.py
+++ wifijammer.py
@@ -160,11 +160,11 @@ def get_iface(interfaces):
         for iface in interfaces:
             interface = iface
             print ('['+R+'-'+W+'] Minor error:',e)
-            print '    Starting monitor mode on '+G+interface+W
+            print ('    Starting monitor mode on '+G+interface+W)
             return interface
 
 def start_mon_mode(interface):
-    print '['+G+'+'+W+'] Starting monitor mode off '+G+interface+W
+    print ('['+G+'+'+W+'] Starting monitor mode off '+G+interface+W)
     try:
         os.system('ip link set %s down' % interface)
         os.system('iwconfig %s mode monitor' % interface)
@@ -184,8 +184,8 @@ def mon_mac(mon_iface):
     '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', mon_iface[:15]))
-    mac = ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
-    print '['+G+'*'+W+'] Monitor mode: '+G+mon_iface+W+' - '+O+mac+W
+    mac = (''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1])
+    print ('['+G+'*'+W+'] Monitor mode: '+G+mon_iface+W+' - '+O+mac+W)
     return mac
 
 ########################################
@@ -220,12 +220,12 @@ def channel_hop(mon_iface, args):
             try:
                 proc = Popen(['iw', 'dev', mon_iface, 'set', 'channel', monchannel], stdout=DN, stderr=PIPE)
             except OSError:
-                print '['+R+'-'+W+'] Could not execute "iw"'
+                print ('['+R+'-'+W+'] Could not execute "iw"')
                 os.kill(os.getpid(),SIGINT)
                 sys.exit(1)
             for line in proc.communicate()[1].split('\n'):
                 if len(line) > 2: # iw dev shouldnt display output unless there's an error
-                    err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
+                    err = ('['+R+'-'+W+'] Channel hopping failed: '+R+line+W)
 
         output(err, monchannel)
         if args.channel:
@@ -287,22 +287,22 @@ def output(err, monchannel):
     if err:
         print err
     else:
-        print '['+G+'+'+W+'] '+mon_iface+' channel: '+G+monchannel+W+'\n'
+        print ('['+G+'+'+W+'] '+mon_iface+' channel: '+G+monchannel+W+'\n')
     if len(clients_APs) > 0:
         print '                  Deauthing                 ch   ESSID'
     # Print the deauth list
     with lock:
         for ca in clients_APs:
             if len(ca) > 3:
-                print '['+T+'*'+W+'] '+O+ca[0]+W+' - '+O+ca[1]+W+' - '+ca[2].ljust(2)+' - '+T+ca[3]+W
+                print ('['+T+'*'+W+'] '+O+ca[0]+W+' - '+O+ca[1]+W+' - '+ca[2].ljust(2)+' - '+T+ca[3]+W)
             else:
-                print '['+T+'*'+W+'] '+O+ca[0]+W+' - '+O+ca[1]+W+' - '+ca[2]
+                print ('['+T+'*'+W+'] '+O+ca[0]+W+' - '+O+ca[1]+W+' - '+ca[2])
     if len(APs) > 0:
         print '\n      Access Points     ch   ESSID'
     with lock:
         for ap in APs:
-            print '['+T+'*'+W+'] '+O+ap[0]+W+' - '+ap[1].ljust(2)+' - '+T+ap[2]+W
-    print ''
+            print ('['+T+'*'+W+'] '+O+ap[0]+W+' - '+ap[1].ljust(2)+' - '+T+ap[2]+W)
+    print ('')
 
 def noise_filter(skip, addr1, addr2):
     # Broadcast, broadcast, IPv6mcast, spanning tree, spanning tree, multicast, broadcast
@@ -458,5 +458,5 @@ if __name__ == "__main__":
     except Exception as msg:
         remove_mon_iface(mon_iface)
         os.system('service network-manager restart')
-        print '\n['+R+'!'+W+'] Closing'
+        print ('\n['+R+'!'+W+'] Closing')
         sys.exit(0)
